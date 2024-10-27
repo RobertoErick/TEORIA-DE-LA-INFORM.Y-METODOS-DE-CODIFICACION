88bc08e04263831595db2e99e71e9afa3de4bfbf
Lordran <lordran@lordran.dev>
2024-10-27 00:47:21 -0600

    Removing line breaks from frequency counting

--- utils/utils.py
+++ utils/utils.py
@@ -2,5 +2,5 @@ import collections
 
 
 def sort_and_order_frequencies(text: str) -> list[tuple[str, int]]:
-    frequency = collections.Counter(text.replace(" ", "").lower())
+    frequency = collections.Counter(text.replace(" ", "").replace("\n","").lower())
     return sorted(frequency.items(), key=lambda x: (-x[1], -ord(x[0])))
