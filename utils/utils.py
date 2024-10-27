cede782c6c9a0126e5dfd5f3f612443ced6c7e24
Lordran <lordran@lordran.dev>
2024-10-27 00:28:33 -0600

    Reordered sorting chars by frequency

---
+++ utils/utils.py
@@ -0,0 +1,6 @@
+import collections
+
+
+def sort_and_order_frequencies(text: str) -> list[tuple[str, int]]:
+    frequency = collections.Counter(text.replace(" ", "").lower())
+    return sorted(frequency.items(), key=lambda x: (-x[1], -ord(x[0])))
