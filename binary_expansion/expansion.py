07eb891fd8206ff294bdd3ebde98a44ff0c72f27
Lordran <lordran@lordran.dev>
2024-10-30 04:00:52 -0600

    Fixed error in Binary Expansion

--- binary_expansion/expansion.py
+++ binary_expansion/expansion.py
@@ -1,27 +1,25 @@
 
 
-def binary_expansion(num: float, tolerance: float = 1e-10):
-    num *= 2
-    expansion = [(num, 0)]
-    history = {num}
+def binary_expansion(num: float, tolerance: float = 1e-10) -> list[int]:
+    expansion = []
+    history = []
 
     while True:
-        if num >= 1:
+        if num >= float(1):
             num = (num - 1) * 2
-            bit = 1
         else:
             num *= 2
-            bit = 0
-
+        bit = 1 if num >= float(1) else 0
         expansion.append((num, bit))
 
         # Check for repeated numbers with a tolerance
         if any(abs(num - h) < tolerance for h in history):
             break
         else:
-            history.add(num)
-
+            history.append(num)
     print(expansion)
+    sequence = [num for _,num in expansion]
+    return sequence
 
 
 def __run__():
